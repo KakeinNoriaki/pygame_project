c39131332c5d86c323ed41357b7696006c155143
KakeinNoriaki <maks.knyazev.05@yandex.ru>
2022-01-21 23:31:22 +0300

    Сартовый экран добавил

--- main.py
+++ main.py
@@ -169,6 +169,32 @@ pygame.mixer.music.play()
 counter = 0
 
 
+def start_screen():
+    intro_text = ['Типо заставка']
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
 def main():
     print(all_sprites)
     running = True
@@ -199,4 +225,5 @@ def main():
 
 
 if __name__ == '__main__':
+    start_screen()
     sys.exit(main())
